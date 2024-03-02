a643fc7bda84efd970e6de2cbec23d67fd223764
Code4Daze <jevon.fillette@gmail.com>
2024-03-02 03:04:37 -0500

    Add login

---
+++ src/components/AddAlert.jsx
@@ -0,0 +1,9 @@
+const AddAlert = () => {
+  return (
+    <div>
+      Form input will be added here
+    </div>
+  )
+}
+
+export default AddAlert;
---
+++ src/components/Login.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+const Login = ({setIsLoggedIn}) => {
+  const loginKey = 'edgewood123!';
+
+  const [data, setData] = useState({
+    password: ""
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (data.password === loginKey) {
+      setIsLoggedIn(true);
+    } else {
+      alert("Wrong password, try again");
+    }
+  }
+
+  const handleChange = (event) => {
+    const newData = {...data};
+    if (event.target.name === "password") {
+      newData.password = event.target.value;
+      setData(newData);
+    }
+  }
+
+  return (
+    <div className="login">
+      <h1>Enter Password to Continue</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          required
+          name="password"
+          id="password"
+          type="text"
+          value={data.password}
+          onChange={handleChange}
+        ></input>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default Login;
